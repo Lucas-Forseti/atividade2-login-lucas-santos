1716c5f9a5fc996903739094b0e768721b2a8a5f
Lucas Oliveira Santos <lucas.santos@forseti.com.br>
2019-12-11 14:26:33 -0300

    Trait implementada para execução dos logs

--- src/Traits/ForsetiLoggerTraits.php
+++ src/Traits/ForsetiLoggerTraits.php
@@ -4,7 +4,14 @@
 namespace Forseti\Bot\PontoLogin\Traits;
 
 
+use Forseti\Logger\Logger;
+use Psr\Log\LoggerTrait;
+
 trait ForsetiLoggerTraits
 {
-
+    use LoggerTrait;
+    public function log($level, $message, array $context = array())
+    {
+        return (new Logger(get_class($this)))->log($level, $message, $context);
+    }
 }
