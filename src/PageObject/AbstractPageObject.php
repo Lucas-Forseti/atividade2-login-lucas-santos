70311d25604eda67145ae8c2aa14d8edf641232c
Lucas Oliveira Santos <lucas.santos@forseti.com.br>
2019-12-12 09:28:45 -0300

    Criando request manualmente

--- src/PageObject/AbstractPageObject.php
+++ src/PageObject/AbstractPageObject.php
@@ -4,11 +4,30 @@
 namespace Forseti\Bot\PontoLogin\PageObject;
 
 
+use Forseti\Bot\PontoLogin\Singleton\GuzzleClientSingleton;
+use Forseti\Bot\PontoLogin\Traits\ForsetiLoggerTraits;
+use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Exception\GuzzleException;
+
 abstract class AbstractPageObject
 {
+    use ForsetiLoggerTraits;
     protected $client;
-    public function __construct()
+    public function __construct(ClientInterface $client = null)
     {
+        $this->client = ($client == null) ? GuzzleClientSingleton::getInstance() : $client;
+    }
 
+    public function request($method, $uri, array $options = [])
+    {
+        try {
+            return $this->client->request($method, $uri, $options);
+        } catch (GuzzleException $e) {
+            $this->error('Erro ao tentar requisicao', ['exception' => $e]);
+            return null;
+        } catch (\Exception $e) {
+            $this->error('Erro ao tentar requisicao', ['exception' => $e]);
+            return null;
+        }
     }
 }
